2aa37ea82117b11d32999d316387c90cc102786b
ryder-mcdowell <ryder.mcdowell@gmail.com>
2019-02-02 21:26:44 -0800

    Add file upload to s3

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 var inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
 
-const ask = async() => {
+const askForFilePath = async() => {
   return inquirer
   .prompt([{
     type: 'input',
-    name: 'question',
-    message: 'Please enter your name:'
+    name: 'filePath',
+    message: 'Enter file path to upload:'
   }])
   .then(answers => {
-    console.log(answers)
+    return answers.filePath
+  });
+}
+
+const uploadFile = async(filePath) => {
+  console.log('reading...')
+  fs.readFile(filePath, function(err, data) {
+    if (err) throw err;
+    
+    const fileName = path.basename(filePath);
+
+    console.log('uploading...')
+    s3.upload({
+      Key: fileName,
+      Bucket: 'testy-tester-351541531532',
+      Body: data,
+      ACL: 'private'
+    }, function (err, data) {
+      if (err) throw err;
+      console.log('file uploaded!');
+    })
   });
 }
 
 const run = async() => {
-  await ask();
+  const filePath = await askForFilePath();
+  await uploadFile(filePath);
 }
 
 run();
